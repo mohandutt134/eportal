20a9b667ef841ad06404b5b643d8f3ddb7b446f7
aman0511 <amankumar0511@gmail.com>
2014-11-06 12:47:12 +0530

    login complete

--- student/views.py
+++ student/views.py
@@ -19,11 +19,11 @@ def home(request):
 	return render(request,'index.html', {'current_date': now})
 
 def login(request):
-	ausername = request.POST.get('username', '')
-	password=request.POST.get('password', '')
-	R_username=request.POST.get('R_username','')
-	R_password=request.POST.get('R_password','')
+	
+	
 	if('login' in request.POST):
+		ausername = request.POST.get('username', '')
+		password=request.POST.get('password', '')
 		if(ausername!='' and password!=''):
 			result=user.objects.filter(username=ausername).values()[0]
 			if(result['password']==password):
@@ -32,6 +32,8 @@ def login(request):
 				return render(request,'login_register.html',{'message':"wrong password"})
 
 	if('register' in request.POST):
+		R_username=request.POST.get('R_username','')
+		R_password=request.POST.get('R_password','')
 		if(R_username!='' and R_password!=''):
 			b = user(username=R_username,password=R_password)
 			b.save()

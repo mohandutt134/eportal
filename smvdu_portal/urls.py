62ccb1f51f669badca89b190702755b721c4b4a8
aman0511 <amankumar0511@gmail.com>
2014-11-13 14:37:45 +0530

    django authtentication

--- smvdu_portal/urls.py
+++ smvdu_portal/urls.py
@@ -7,11 +7,11 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'student.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-     url(r'^login$', 'student.views.login', name='login'),
+     url(r'^login$', 'student.views.login_view', name='login'),
      url(r'^course$', 'course.views.course', name='courses'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^faculty$', 'student.views.faculty', name='faculty'),
     url(r'^about$', 'smvdu_portal.views.about', name='about'),
-    url(r'^logout$', 'student.views.logout', name='logout'),
+    url(r'^logout$', 'student.views.logout_view', name='logout'),
     url(r'^quiz$', 'quiz.views.quiz', name='quiz'),
 )

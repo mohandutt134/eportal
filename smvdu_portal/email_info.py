6f1f6e4247e6b5fb35ecae2f77092e266c956f38
Vibhanshu Chaturvedi <vibhanshu86@gmail.com>
2014-12-06 20:34:44 +0530

    V2.1

--- smvdu_portal/email_info.py
+++ smvdu_portal/email_info.py
@@ -1,7 +1,7 @@
 #for email verification
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'smvdueportal@gmail.com'
-EMAIL_HOST_PASSWORD = '!@#$%^zxcv'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 25
 
